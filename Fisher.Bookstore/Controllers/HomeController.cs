deafd04a56ec688264aa614b4135fd3d6f7700d0
Max McElligott <mcelligott.18@osu.edu>
2019-01-23 21:08:35 -0500

    home-controller

---
+++ Fisher.Bookstore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fisher.Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {   public IActionResult Index()
+        {
+            return Content("Hello World");
+        }
+    }
+}
