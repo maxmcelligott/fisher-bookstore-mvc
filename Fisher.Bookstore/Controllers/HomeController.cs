ec29e97e4a72bd8aef19fc3e201d7c1f30fd1b3f
Max McElligott <mcelligott.18@osu.edu>
2019-01-23 21:26:39 -0500

    new-control

--- Fisher.Bookstore/Controllers/HomeController.cs
+++ Fisher.Bookstore/Controllers/HomeController.cs
@@ -4,9 +4,25 @@ using Microsoft.AspNetCore.Mvc;
 namespace Fisher.Bookstore.Controllers
 {
     public class HomeController : Controller
-    {   public IActionResult Index()
+    {  
+         public IActionResult Index()
         {
             return Content("Hello World");
+        }
+    }
+    public class AboutController : Controller
+    {  
+         public IActionResult Index()
+        {
+            return Content("This is index");
+        }
+        public IActionResult History()
+        {
+            return Content("This is history");
+        }
+        public IActionResult Location()
+        {
+            return Content("This is location");
         }
     }
 }
