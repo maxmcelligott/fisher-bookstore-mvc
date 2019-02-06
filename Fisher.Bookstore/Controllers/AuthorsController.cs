a6fae8a1c42d61c4c62dc875ee06a3a8e6603e4f
Max McElligott <mcelligott.18@osu.edu>
2019-02-06 16:07:36 -0500

    updated file

--- Fisher.Bookstore/Controllers/AuthorsController.cs
+++ Fisher.Bookstore/Controllers/AuthorsController.cs
@@ -3,11 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Fisher.Bookstore.Controllers
 {
-    public class HomeController : Controller
-    {  
+        public class AuthorsController : Controller
+         {  
          public IActionResult Index()
         {
             return View();
         }
-    }
-}
+        public IActionResult Featured()
+        {
+            return View();
+        }
+        }}
